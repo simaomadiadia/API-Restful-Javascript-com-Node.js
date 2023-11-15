7222bd7a6166506b89fd18df7edbc885ffd0daf3
Simão Madiadia <simaomadiadia@gmail.com>
2023-11-15 22:36:27 +0000

    fist commit

---
+++ src/shared/http/Server.ts
@@ -0,0 +1,31 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-error';
+import cors from 'cors';
+
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+// Criacao do middlew
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        statu: 'error',
+        message: error.message,
+      });
+    }
+    return response.status(500).json({
+      statu: 'error',
+      message: 'internal server error',
+    });
+  },
+);
+app.listen(1024, () => {
+  console.log('Servidor rodando na porta 1024');
+});
---
+++ src/shared/http/routes/index.ts
@@ -0,0 +1,12 @@
+import productsRouter from '@modules/products/routes/products.routes';
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.use('/products', productsRouter);
+
+/*routes.get('/', (request, response) => {
+  return response.json({ messagem: 'Ola mundo' });
+});
+*/
+export default routes;
